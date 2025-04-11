406ca24fee3a6914db4d9a37d0edc958981e9462
Mohammed0K <M.H.D-6@hotmail.com>
2025-04-11 22:22:19 +0000

    update v3

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -10,20 +10,30 @@ void merge_sort(int *array, size_t size)
 
 	if (!array || size < 2)
 		return;
+
 	tmp = malloc(sizeof(int) * size);
 	if (!tmp)
 		return;
+
 	split(array, tmp, 0, size);
 	free(tmp);
 }
 
-/* Helper: Recursively splits and merges */
+/**
+ * split - Recursively splits and merges array
+ * @a: Array to sort
+ * @t: Temp buffer
+ * @l: Left index
+ * @r: Right index
+ */
 void split(int *a, int *t, size_t l, size_t r)
 {
 	size_t m;
+
 	if (r - l > 1)
 	{
 		m = (l + r) / 2;
+
 		split(a, t, l, m), split(a, t, m, r);
 		printf("Merging...\n[left]: "), print_array(a + l, m - l);
 		printf("[right]: "), print_array(a + m, r - m);
@@ -32,10 +42,18 @@ void split(int *a, int *t, size_t l, size_t r)
 	}
 }
 
-/* Helper: Merges two subarrays */
+/**
+ * merge - Merges two sorted subarrays
+ * @a: Array
+ * @t: Temp buffer
+ * @l: Left
+ * @m: Mid
+ * @r: Right
+ */
 void merge(int *a, int *t, size_t l, size_t m, size_t r)
 {
 	size_t i = l, j = m, k = l;
+
 	while (k < r)
 		t[k++] = (j == r || (i < m && a[i] <= a[j])) ? a[i++] : a[j++];
 	for (k = l; k < r; k++)
--- sort.h
+++ sort.h
@@ -34,4 +34,22 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+/**
+ * split - Recursively splits array and merges it
+ * @a: Array to sort
+ * @t: Temp buffer
+ * @l: Left index
+ * @r: Right index
+ */
+void split(int *a, int *t, size_t l, size_t r);
+
+/**
+ * merge - Merges two subarrays into one
+ * @a: Array to sort
+ * @t: Temp buffer
+ * @l: Left start
+ * @m: Middle
+ * @r: Right end
+ */
+void merge(int *a, int *t, size_t l, size_t m, size_t r);
 #endif /* SORT_H */
