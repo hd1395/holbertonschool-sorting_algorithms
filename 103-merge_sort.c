36d72b0b8b6d4c4d9b6fa489487196730da988bc
Mohammed0K <M.H.D-6@hotmail.com>
2025-04-11 22:16:23 +0000

    update v2

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,46 +1,43 @@
 #include "sort.h"
-#include <stdlib.h>
-#include <stdio.h>
-
 /**
- * merge_sort - Sorts array with top-down merge sort
+ * merge_sort - Sorts array using merge sort
  * @array: Array to sort
- * @size: Array size
+ * @size: Size of the array
  */
 void merge_sort(int *array, size_t size)
 {
 	int *tmp;
-	void merge(int *, int *, size_t, size_t, size_t);
 
 	if (!array || size < 2)
 		return;
-
 	tmp = malloc(sizeof(int) * size);
 	if (!tmp)
 		return;
+	split(array, tmp, 0, size);
+	free(tmp);
+}
 
-	void split(int *a, int *t, size_t l, size_t r)
+/* Helper: Recursively splits and merges */
+void split(int *a, int *t, size_t l, size_t r)
+{
+	size_t m;
+	if (r - l > 1)
 	{
-		size_t m;
-		if (r - l > 1)
-		{
-			m = (l + r) / 2;
-			split(a, t, l, m), split(a, t, m, r);
-			printf("Merging...\n[left]: ");
-			print_array(a + l, m - l), printf("[right]: ");
-			print_array(a + m, r - m);
-			merge(a, t, l, m, r);
-			printf("[Done]: ");
-			print_array(a + l, r - l);
-		}
+		m = (l + r) / 2;
+		split(a, t, l, m), split(a, t, m, r);
+		printf("Merging...\n[left]: "), print_array(a + l, m - l);
+		printf("[right]: "), print_array(a + m, r - m);
+		merge(a, t, l, m, r);
+		printf("[Done]: "), print_array(a + l, r - l);
 	}
-	void merge(int *a, int *t, size_t l, size_t m, size_t r)
-	{
-		size_t i = l, j = m, k = l;
-		while (k < r)
-			t[k++] = (j == r || (i < m && a[i] <= a[j])) ? a[i++] : a[j++];
-		for (k = l; k < r; k++)
-			a[k] = t[k];
-	}
-	split(array, tmp, 0, size), free(tmp);
+}
+
+/* Helper: Merges two subarrays */
+void merge(int *a, int *t, size_t l, size_t m, size_t r)
+{
+	size_t i = l, j = m, k = l;
+	while (k < r)
+		t[k++] = (j == r || (i < m && a[i] <= a[j])) ? a[i++] : a[j++];
+	for (k = l; k < r; k++)
+		a[k] = t[k];
 }
